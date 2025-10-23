fccc3609c8869abdd46b75cb1014a5d82397801a
wyattYousey <quiz.nv1@gmail.com>
2025-10-23 16:59:38 -0500

    Add modal functionality and responsive styles
    Introduced JavaScript to handle opening and closing of the Edit Profile and New Post modals. Updated modal HTML to include unique IDs and script reference. Enhanced modal CSS for better responsiveness on smaller screens and improved layout consistency.

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileSaveBtn = editProfileModal.querySelector(".modal__save-btn");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostBtn = document.querySelector(".profile__new-post-btn");
+const newPostSaveBtn = newPostModal.querySelector(".modal__save-btn");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+editProfileCloseBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+newPostBtn.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
+});
+
+newPostCloseBtn.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
+});
