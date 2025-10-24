e217ea36d2472f8dd1e919b476c1c2ba383a84fd
wyattYousey <quiz.nv1@gmail.com>
2025-10-23 20:19:20 -0500

    Add modal transitions and form handling for profile and posts
    Introduces opacity transitions to modal appearance for smoother UX. Adds JavaScript logic to handle profile edit and new post form submissions, updating profile info and logging new post data, and closes modals upon submission.

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,21 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileSaveBtn = editProfileModal.querySelector(".modal__save-btn");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostSaveBtn = newPostModal.querySelector(".modal__save-btn");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+
+const profileName = document.querySelector(".profile__header-title");
+const profileNameInput = editProfileModal.querySelector("#name");
+const profileDescription = document.querySelector(".profile__header-subtitle");
+const profileDescriptionInput = editProfileModal.querySelector("#description");
+
+const postImgInput = newPostModal.querySelector("#image-link");
+const postCaptionInput = newPostModal.querySelector("#caption");
 
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
@@ -22,3 +33,24 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+profileNameInput.value = profileName.textContent;
+profileDescriptionInput.value = profileDescription.textContent;
+
+function handleEditFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditFormSubmit);
+
+function handlePostFormSubmit(event) {
+  event.preventDefault();
+  console.log(postImgInput.value);
+  console.log(postCaptionInput.value);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+newPostForm.addEventListener("submit", handlePostFormSubmit);
